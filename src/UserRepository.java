7639a813586a7351e11a24b394e232ffc00ae70b
MarkNash18 <74380430+MarkNash18@users.noreply.github.com>
2020-11-12 21:52:00 +0000

    Revert "General Tidy Up"
    This reverts commit 766fa5ce

--- src/UserRepository.java
+++ src/UserRepository.java
@@ -1,27 +1,21 @@
-package GroupProject.UUGroup13.src;
-
-/**
- * Define a UserRepository Class
- */
-
 public class UserRepository {
     private int nextArrayIndex = 0;
     private User[] users;
 
     public UserRepository(){
         this.users = new User[1000];
-    }//UserRepository
+    }
 
     public User[] getUsers() {
         return this.users;
-    }//getUsers
+    }
 
-    public void addUser(User newUser){
+    public void addUser( User newUser ){
         this.users[this.nextArrayIndex] = newUser;
         this.nextArrayIndex++;
-    }//addUser
+    }
 
     public int getNextArrayIndex() {
         return nextArrayIndex;
-    }//getNextArrayIndex
-}//class
+    }
+}
